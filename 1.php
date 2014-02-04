30ba4dee22e5562a9792a0ed3f388d466a8aaf08
absent1706 <litvinenko1706@gmail.com>
2014-02-05 01:18:11 +0200

    Основные функции работают. Теперь надо добавить проверялки урлов и кодировок страницы

--- 1.php
+++ 1.php
@@ -1,45 +1,52 @@
-<html>
+<?php
+header('Content-Type: text/html; charset=utf-8');
+include 'simple_html_dom.php';
+
+
+if (isset($_POST['run'])){
+	$url=$_POST['url'];
+	$keyword=$_POST['keyword'];
+} else{
+	$url="http://i-vd.org.ru/books/html-begin/example02.html";
+	$keyword="по";
+}
+print  '<html>
          <head></head>
          <body>
-<?php
-include 'simple_html_dom.php';
-$url="http://vk.com";//http://i-vd.org.ru/books/html-begin/example02.html";
-$keyword="word";
-
-print  '
 			
-			<form action="'.$_SERVER['PHP_SELF'].'">
+			<form action="'.$_SERVER['PHP_SELF'].'" method="post">
 				<input type="text" name="keyword" value="'.$keyword.'"><br/>
 				<input type="text" size="100" name="url" value="'.$url.'"><br/>
-				<input type="submit" name="send">
+				<input type="submit" name="run">
 			</form>
-			'.count_keyword_at_url($url,$keyword).'
+			<br/>
 ';
 
+
+print count_keyword_at_url($url,$keyword);
+
 function count_keyword_at_url($url,$keyword){
-	 $html = new simple_html_dom();
-	 $html->load_file($url);
-	 $head = $html->find('body');
-	echo htmlspecialchars($head[0]->innertext,NULL,'');
 
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_HEADER, 1);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);	
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	$html=curl_exec($ch);
+	curl_close($ch);
 
-	//preg_match_all('#<head>(.*)</head>#',$page,$head);
-	//var_dump($head);
+	echo '<pre>'.htmlspecialchars($html,NULL,'').'</pre>';
+	$page=str_get_html($html);
+	$head = $page->find('body')[0]->innertext;
 
-	//echo $head->innertext;
-
-//$page=str_replace('<', '>', $page);
-	//$head=htmlspecialchars($head,NULL,'');
+	//Определение кодировки страницы
 	
+	if (mb_detect_encoding($head) != ) ? mb_detect_encoding($head) : '';
+	//$head=iconv()
 
-	//$page=htmlspecialchars($page,NULL,'');
-	//var_dump($page);
-	//return 'e';
-//	$html->clear();
-// Удаление DOM обьекта
-//unset($html);
-	//$rss =  simplexml_load_file($url);
-//	echo $rss;
+	preg_match_all('#'.preg_quote($keyword).'#', $html,$res);
+	//print_r($res);
+	return count($res[0]);
+
 }
 
 ?>		 </body>	
